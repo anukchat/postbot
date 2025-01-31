771bd4dcf69f1a613d30091bbfc9d47b16cd7b8c
Anukool Chaturvedi <chaturvedianukool@gmail.com>
2025-01-31 20:34:15 +0530

    fixed error

--- src/backend/api.py
+++ src/backend/api.py
@@ -958,5 +958,5 @@ async def get_content_by_thread(
 
 
 if __name__ == "__main__":
-    uvicorn.run("api:app", host="0.0.0.0", port=8000, reload=False)
+    uvicorn.run("src.backend.api:app", host="0.0.0.0", port=8000, reload=False)
 
